10cc04a50b1d5c063ae6fcf81513982d32b699a9
Ashley Hollis <AshleyHollis@github.com>
2015-04-04 22:51:37 +1000

    Added BurgerShopInitializer.cs

---
+++ BurgerShop.Data/BurgerShopInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurgerShop.Data
+{
+    internal class BurgerShopInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<BurgerShopContext>
+    {
+        protected override void Seed(BurgerShopContext context)
+        {
+            var products = new List<Product>
+            {
+                new Product {ID = 1, Name = "Product01", ProductCode = "P001"}
+
+            };
+
+            products.ForEach(s => context.Products.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
