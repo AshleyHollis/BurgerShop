a41a491cef1f1fbbca0a2ab077788ca41e2dc223
Ashley Hollis <AshleyHollis@github.com>
2015-04-04 22:40:14 +1000

    Configured EF and added Product entity class.

---
+++ BurgerShop.Data/BurgerShopContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurgerShop.Data
+{
+    internal class BurgerShopContext : DbContext
+    {
+        public BurgerShopContext() : base("BurgerShopContext")
+        {}
+
+        //public DbSet<aspnet_Applications> aspnet_Applications { get; set; }
+        //public DbSet<aspnet_Users> aspnet_Users { get; set; }
+        //public DbSet<aspnet_Membership> aspnet_Membership { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ BurgerShop.Data/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace BurgerShop.Data
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string ProductCode { get; set; }
+
+        public string Name { get; set; }
+    }
+}
